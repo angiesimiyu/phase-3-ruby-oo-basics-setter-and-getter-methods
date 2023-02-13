cda124030f03a4500278a05553b8f647fa61d4fa
angelinesimiyu <angie.lukelas@gmail.com>
2023-02-13 14:02:00 +0300

    Completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
 class Person
+    # setter method
+  def name=(name)
+    @name = name
+  end
+
+  def job= (job)
+    @job = job
+  end
+
+  # getter method
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
 
 end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+
+singer = Person.new
+singer.job = "Singer"
+
